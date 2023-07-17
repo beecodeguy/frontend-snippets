cdf9ab35aeb0434db9070ecb224956d0a8e98c07
okur <bijay.okur@gmail.com>
2023-07-17 22:05:31 +0545

    build: copy to clipboard done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
-import styles from "app/page.module.css";
-import ThemeSwitch from "components/common/ThemeSwitch/ThemeSwitch";
+"use client";
+import { useState } from "react";
+import { BiCopy } from "react-icons/bi";
+import { BsCheckAll } from "react-icons/bs";
 import { Input } from "shad/components/ui/input";
 
 export default function Home() {
+  const [copied, setCopied] = useState("");
+  const [text, setText] = useState("");
+
+  const handleInputChange = e => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  const handleCopy = () => {
+    if (text) {
+      setCopied(text);
+      navigator.clipboard.writeText(text);
+      setTimeout(() => setCopied(""), 3000);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <h1 className="text-center text-3xl underline">All the pages here</h1>
-      <ThemeSwitch />
-      <Input placeholder="from shadcn/ui (Type here...)" />
+    <main className="main-container">
+      <div className="p-5 flex gap-4 w-90">
+        <Input placeholder="Enter the text here...." onChange={handleInputChange} />
+        <div
+          className="copy-container flex border rounded p-2 min-w-fit items-center gap-2 cursor-pointer"
+          onClick={() => handleCopy()}
+        >
+          <span className="text-[10px]">{copied ? "Copied" : "Copy to Clipboard"}</span>
+          <span>{copied ? <BsCheckAll /> : <BiCopy />}</span>
+        </div>
+      </div>
     </main>
   );
 }
